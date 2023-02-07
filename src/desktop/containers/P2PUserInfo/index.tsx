252ebdde7fb6ae7d061b616db3a9fc9e80177965
naufalarifk <abnormalchild414@gmail.com>
2023-02-07 11:11:40 +0700

    fixed modal on changing username

--- src/desktop/containers/P2PUserInfo/index.tsx
+++ src/desktop/containers/P2PUserInfo/index.tsx
@@ -7,21 +7,34 @@ import { FormControl, Modal } from 'react-bootstrap';
 export const P2PUserInfo: React.FC = () => {
     const [username, setUsername] = React.useState('Nusatech Exchange');
     const [showChangeUsernameModal, setShowChangeUsernameModal] = React.useState(false)
-    const userP2P = useSelector(selectP2PAccount)
-    console.log(userP2P); 
+    // const userP2P = useSelector(selectP2PAccount)
+    // console.log(userP2P); 
     const ModalChangeName = () => {
         return (
-            <div hidden={!showChangeUsernameModal}>
-                <h1>Input to Change Username</h1>
-                <FormControl
-                key='username'
-                type='text'
-                value={username}
-                onChange={(e)=> setUsername(e.target.value)}
-                autoFocus={true}
-                />
-                <div>Confirm</div>
-            </div>
+            <form onSubmit={()=> setShowChangeUsernameModal(false)} className='bg-black p-10 pt-20' hidden={!showChangeUsernameModal}>
+                <div className='d-flex justify-content-between'>
+                    <h6 className='text-white my-20'>Set Nickname</h6>
+                    <svg onClick={()=> setShowChangeUsernameModal(false)} className='cursor-pointer' width="20px" height="20px" viewBox="0 0 24 24" fill="#FFFFFF" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M5.29289 5.29289C5.68342 4.90237 6.31658 4.90237 6.70711 5.29289L12 10.5858L17.2929 5.29289C17.6834 4.90237 18.3166 4.90237 18.7071 5.29289C19.0976 5.68342 19.0976 6.31658 18.7071 6.70711L13.4142 12L18.7071 17.2929C19.0976 17.6834 19.0976 18.3166 18.7071 18.7071C18.3166 19.0976 17.6834 19.0976 17.2929 18.7071L12 13.4142L6.70711 18.7071C6.31658 19.0976 5.68342 19.0976 5.29289 18.7071C4.90237 18.3166 4.90237 17.6834 5.29289 17.2929L10.5858 12L5.29289 6.70711C4.90237 6.31658 4.90237 5.68342 5.29289 5.29289Z" fill="#606060"/>
+                    </svg>
+                </div>
+                <p className='text-secondary'>It is recommended not to use your real name. <br/> Nickname can only be modified after KYC level 3.</p>
+                <label className='text-white'>Nickname</label>
+                <div className='d-flex flex-row justify-content-between modal-input border border-white rounded-lg'>
+                    <FormControl
+                    key='username'
+                    type='text'
+                    placeholder='Enter your nickname'
+                    value={username}
+                    onChange={(e)=> setUsername(e.target.value)}
+                    autoFocus={true}
+                    maxLength={20}
+                    className='bg-transparent border-0 border-bottom border-white text-white w-70'
+                    />
+                    <label className='text-char'>{username.length}/20</label>
+                </div>
+                <div className='text-center cursor-pointer btn-primary' onClick={()=> setShowChangeUsernameModal(false)}>OK</div>
+            </form>
         )
     }
     
