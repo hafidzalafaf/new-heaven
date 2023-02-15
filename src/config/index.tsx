2252cba4886f96026cb4cbcda53927b19d97ef02
naufalarifk <abnormalchild414@gmail.com>
2023-02-15 11:47:46 +0700

    fixed announcement slider items

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -1,4 +1,9 @@
-const USE_SSL = window.location.protocol === 'https:'
+// use this file to set the API URL and SOCKET URL on prod
+// const USE_SSL = window.location.protocol === 'https:'
+
+// use this file to set the API URL and SOCKET URL on dev
+const USE_SSL = true
+
 const BASE_URL = 'dev.heavenexchange.io';
 // const BASE_URL = 'api.heavenexchange.io';
 
