8cdcca694d1e126380d71cb10b376f1e95aace0c
devops-nusatech <devops@nusatech.id>
2023-04-17 10:20:53 +0700

    config dev local

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -4,7 +4,7 @@
 
 // use this file to set the API URL and SOCKET URL on dev
 const USE_SSL = true;
-const BASE_URL = 'dev.heavenexchange.io';
+const BASE_URL = 'dev.nusatech.io';
 
 export const API_URL = USE_SSL ? 'https://' + BASE_URL : 'http://' + BASE_URL;
 export const SOCKET_URL = USE_SSL ? 'wss://' + BASE_URL : 'ws://' + BASE_URL;
