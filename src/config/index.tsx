9a4d15a484cdedd801ca786c6e1d238bb5bfe95f
devops-nusatech <devops@nusatech.id>
2023-08-12 10:24:37 +0700

    config url prod

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -1,12 +1,12 @@
 // use this file to set the API URL and SOCKET URL on prod
-// const USE_SSL = window.location.protocol === 'https:';
-// const BASE_URL = 'www.heavenexchange.io';
-
-// use this file to set the API URL and SOCKET URL on dev
-const USE_SSL = true;
+const USE_SSL = window.location.protocol === 'https:';
 const BASE_URL = 'www.heavenexchange.io';
 
-export const CURRENT_VERSION = '3.1.0';
+// use this file to set the API URL and SOCKET URL on dev
+// const USE_SSL = true;
+// const BASE_URL = 'www.heavenexchange.io';
+
+export const CURRENT_VERSION = '3.0.1';
 export const GOOGLE_PLAY_LINK = 'https://play.google.com/store/apps/details?id=io.heavenexchange.mobile&pli=1';
 
 export const API_URL = USE_SSL ? 'https://' + BASE_URL : 'http://' + BASE_URL;
